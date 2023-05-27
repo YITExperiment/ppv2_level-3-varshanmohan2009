3be8d04c25533f84452d1e91d823b1d5864daf03
varshanmohan2009 <134772205+varshanmohan2009@users.noreply.github.com>
2023-05-27 16:12:21 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,5 @@
+Python 3.11.0 (main, Oct 24 2022, 18:13:38) [MSC v.1933 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age=12
+>>> print(age)
+12
